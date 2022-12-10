36545ecf87522fa01099c6eb1adcab7797410af0
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2022-12-11 01:11:56 +0600

    done modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,38 +1,34 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  handleCloseClick = e => {
+export const Modal = ({ onClose, image }) => {
+  const handleCloseClick = e => {
     if (e.target.tagName !== 'IMG') {
-      this.props.onClose();
+      onClose();
     }
   };
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
 
-  handleKeyDown = e => {
-    if (e.key === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  render() {
-    return (
-      <div className={css.overlay} onClick={this.handleCloseClick}>
-        <div className={css.modal}>
-          <img src={this.props.image} alt="" />
-        </div>
+  return (
+    <div className={css.overlay} onClick={handleCloseClick}>
+      <div className={css.modal}>
+        <img src={image} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   image: PropTypes.string.isRequired,
